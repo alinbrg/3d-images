225ceae4a2199bcb99aebe1db721ec695dc5c1dd
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-04-02 22:52:44 +0400

    basic layout without functionality

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
 
-export { CustomButton };
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab };
