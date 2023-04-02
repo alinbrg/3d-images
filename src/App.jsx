a21e15392639c047a1a4a563fb970378672dc6de
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-04-02 19:59:52 +0400

    added main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 function App() {
 	return (
-		<div className="App">
-			<h1 className="head-text">React app</h1>
-		</div>
+		<main className="app transition-all ease-in">
+			<Home />
+			<Canvas />
+			<Customizer />
+		</main>
 	);
 }
 
