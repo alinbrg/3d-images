e0eadacce5dea57ed0782cc55613fc6769de0d2a
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-04-04 11:28:53 +0400

    shirt part 1 done

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -8,20 +8,45 @@ export default function Shirt() {
 	const snap = useSnapshot(state);
 	const { nodes, materials } = useGLTF("/shirt_baked.glb");
 
-	console.log(nodes, materials);
+	// console.log(nodes, materials);
 
 	const logoTexture = useTexture(snap.logoDecal);
 	const fullTexture = useTexture(snap.fullDecal);
 
+	useFrame((state, delta) =>
+		easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+	);
+	const stateString = JSON.stringify(snap);
+
 	return (
-		<group>
+		<group key={stateString}>
 			<mesh
 				castShadow
 				geometry={nodes.T_Shirt_male.geometry}
 				material={materials.lambert1}
 				material-roughness={1}
 				dispose={null}
-			></mesh>
+			>
+				{snap.isFullTexture && (
+					<Decal
+						position={[0, 0, 0]}
+						rotation={[0, 0, 0]}
+						scale={1}
+						map={fullTexture}
+					/>
+				)}
+				{snap.isLogoTexture && (
+					<Decal
+						position={[0.08, 0.07, 0.15]}
+						rotation={[0, 0, 0]}
+						scale={0.1}
+						map={logoTexture}
+						map-anisotropy={16}
+						depthTest={false}
+						depthWrite={true}
+					/>
+				)}
+			</mesh>
 		</group>
 	);
 }
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -17,6 +17,28 @@ import {
 
 export default function Customizer() {
 	const snap = useSnapshot(state);
+	const [file, setFile] = useState("");
+	const [prompt, setPrompt] = useState("");
+	const [generatingImg, setGeneratingImg] = useState(false);
+	const [activeEditorTab, setActiveEditorTab] = useState("");
+	const [activeFilterTab, setActiveFilterTab] = useState({
+		logoShirt: true,
+		stylishShirt: false,
+	});
+
+	function generateTabContent() {
+		switch (activeEditorTab) {
+			case "colorpicker":
+				return <ColorPicker />;
+			case "filepicker":
+				return <FilePicker />;
+			case "aipicker":
+				return <AIPicker />;
+			default:
+				return null;
+		}
+	}
+
 	return (
 		<AnimatePresence>
 			{!snap.intro && (
@@ -29,8 +51,16 @@ export default function Customizer() {
 						<div className="flex items-center min-h-screen">
 							<div className="editortabs-container tabs">
 								{EditorTabs.map((tab) => (
-									<Tab key={tab.name} tab={tab} handleClick={() => {}} />
+									<Tab
+										key={tab.name}
+										tab={tab}
+										handleClick={() => {
+											setActiveEditorTab(tab.name);
+										}}
+									/>
 								))}
+
+								{generateTabContent()}
 							</div>
 						</div>
 					</motion.div>
