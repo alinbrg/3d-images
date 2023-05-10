854d4219dfc2016c769d71e6b3025636b1d2b764
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-05-10 19:28:09 +0400

    edit settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "https://github.com/alinbrg/3d-images",
+	base: "/3d-images/",
 	plugins: [react()],
 });
