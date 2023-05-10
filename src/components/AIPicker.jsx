a5cd835961827eb9add3a18b7be4e0efa9b4239d
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-05-10 18:10:12 +0400

    ai picker, doesn't work free

--- src/components/AIPicker.jsx
+++ src/components/AIPicker.jsx
@@ -1,3 +1,47 @@
-export default function AIPicker() {
-	return;
+import CustomButton from "./CustomButton";
+
+export default function AIPicker({
+	prompt,
+	setPrompt,
+	generatingImg,
+	handleSubmit,
+}) {
+	return (
+		<div className="aipicker-container">
+			<textarea
+				name=""
+				id=""
+				cols="30"
+				placeholder="Ask AI..."
+				rows="5"
+				value={prompt}
+				onChange={(e) => setPrompt(e.target.value)}
+				className="aipicker-textarea"
+			></textarea>
+			<div className="flex flex-wrap gap-3">
+				{generatingImg ? (
+					<CustomButton
+						type="outlined"
+						title="Asking Ai..."
+						customStyles="text-xs"
+					/>
+				) : (
+					<>
+						<CustomButton
+							type="outlined"
+							title="Ai Logo"
+							customStyles="text-xs"
+							handleClick={() => handleSubmit("logo")}
+						/>
+						<CustomButton
+							type="filled"
+							title="Ai Full"
+							customStyles="text-xs"
+							handleClick={() => handleSubmit("full")}
+						/>
+					</>
+				)}
+			</div>
+		</div>
+	);
 }
